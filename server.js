d984bc3aa60f1d028529934840689a068134a65c
riachh <59855086+riachh@users.noreply.github.com>
2022-04-26 18:27:23 -0400

    Added help text I think

--- server.js
+++ server.js
@@ -5,6 +5,29 @@ const app = express()
 const args = require('minimist')(process.argv.slice(2))
 args["port"]
 
+// Store help text 
+const help = (`
+server.js [options]
+
+--port	Set the port number for the server to listen on. Must be an integer
+            between 1 and 65535.
+
+--debug	If set to true, creates endlpoints /app/log/access/ which returns
+            a JSON access log from the database and /app/error which throws 
+            an error with the message "Error test successful." Defaults to 
+            false.
+
+--log		If set to false, no log files are written. Defaults to true.
+            Logs are always written to database.
+
+--help	Return this message and exit.
+`)
+// If --help or -h, echo help text to STDOUT and exit
+if (args.help || args.h) {
+    console.log(help)
+    process.exit(0)
+}
+
 //Start Listening 
 const HTTP_PORT = args.port || process.env.PORT || 5000
 const server = app.listen(HTTP_PORT, () => {
