366c99fef236368e8cabcf59a4730485bde63aa4
riachh <59855086+riachh@users.noreply.github.com>
2022-04-27 01:20:53 -0400

    Old commit Server file only

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 //Require Mninmist 
 const args = require('minimist')(process.argv.slice(2))
-//args["port", "debug", "log", "help"]
+args["port", "debug", "log", "help"]
 //Require database
 const db = require('./database.js')
 //Require morgan
@@ -17,18 +17,14 @@ app.use(express.urlencoded({ extended: true}));
 //Store help text 
 const help = (`
 server.js [options]
-
 --port	Set the port number for the server to listen on. Must be an integer
             between 1 and 65535.
-
 --debug	If set to true, creates endlpoints /app/log/access/ which returns
             a JSON access log from the database and /app/error which throws 
             an error with the message "Error test successful." Defaults to 
             false.
-
 --log		If set to false, no log files are written. Defaults to true.
             Logs are always written to database.
-
 --help	Return this message and exit.
 `)
 
@@ -45,7 +41,7 @@ const server = app.listen(HTTP_PORT, () => {
     console.log('App listening on port %PORT%'.replace('%PORT%',HTTP_PORT))
 });
 
-//Check w morgan...DONT CHANGE
+//Check w morgan
 if (args.log) { 
     //Create a write stream to append (flags: 'a') to a file
     const access = fs.createWriteStream('access.log', { flags: 'a' })
@@ -88,7 +84,7 @@ if (args.debug) {
       try{
         const fr = db.prepare('SELECT * FROM accesslog').all()
         res.status(200).json(fr)
-      } catch(e) {
+      } catch (e){
         console.error(e);
       }
     });
